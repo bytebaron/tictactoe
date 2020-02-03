77197acd6cd708c7e16056ee08510cba35701900
bytebaron <bytebaron2000@gmail.com>
2020-02-03 20:11:57 +0000

    Added a grid state to the board.js component. Passed a editGrid prop to the space components. Can now edit the state of board.

--- src/Board.js
+++ src/Board.js
@@ -3,31 +3,44 @@ import Container from 'react-bootstrap/Container';
 import Space from './Space';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 import './globalstyles.css';
 
 class Board extends Component {
     constructor(props) {
         super(props);
-        //this.state;
+        this.state = {
+            1:'', 2:'', 3:'',
+            4:'', 5:'', 6:'',
+            7:'', 8:'', 9:'',
+        }
+        this.editGrid = this.editGrid.bind(this);
+    }
+
+    editGrid(pos) {
+        console.log(pos);
+        this.setState({
+            [pos]:'x',
+        })
     }
 
     render() {
         return (<Container fluid>
             <Row noGutters>
-                <Col md="auto" className="col-edit"><Space /></Col>
-                <Col md="auto" className="col-edit"><Space /></Col>
-                <Col md="auto" className="col-edit"><Space /></Col>
+                <Col md="auto" className="col-edit"><Space pos={1} editGrid={this.editGrid} /></Col>
+                <Col md="auto" className="col-edit"><Space pos={2} editGrid={this.editGrid}/></Col>
+                <Col md="auto" className="col-edit"><Space pos={3} editGrid={this.editGrid}/></Col>
             </Row>
             <Row noGutters>
-                <Col md="auto" className="col-edit"><Space /></Col>
-                <Col md="auto" className="col-edit"><Space /></Col>
-                <Col md="auto" className="col-edit"><Space /></Col>
+                <Col md="auto" className="col-edit"><Space pos={4} editGrid={this.editGrid}/></Col>
+                <Col md="auto" className="col-edit"><Space pos={5} editGrid={this.editGrid}/></Col>
+                <Col md="auto" className="col-edit"><Space pos={6} editGrid={this.editGrid}/></Col>
             </Row>
             <Row noGutters>
-                <Col md="auto" className="col-edit"><Space /></Col>
-                <Col md="auto" className="col-edit"><Space /></Col>
-                <Col md="auto" className="col-edit"><Space /></Col>
+                <Col md="auto" className="col-edit"><Space pos={7} editGrid={this.editGrid}/></Col>
+                <Col md="auto" className="col-edit"><Space pos={8} editGrid={this.editGrid}/></Col>
+                <Col md="auto" className="col-edit"><Space pos={9} editGrid={this.editGrid}/></Col>
             </Row>
         </Container>)
     }
--- src/Space.js
+++ src/Space.js
@@ -7,10 +7,16 @@ class Space extends Component {
     constructor(props) {
         super(props);
         //this.state;
+        this.makeMove = this.makeMove.bind(this);
+    }
+
+    makeMove() {
+        console.log(this.props.pos);
+        this.props.editGrid(this.props.pos);
     }
 
     render() {
-        return (<Container className ="space border border-dark">
+        return (<Container onClick={() => this.makeMove()}className ="space border border-dark">
             
         </Container>)
     }
