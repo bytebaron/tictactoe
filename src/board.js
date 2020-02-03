9259d20f27021da82a430891798b526733eeae10
bytebaron <bytebaron2000@gmail.com>
2020-02-03 18:57:49 +0000

    Removed some boiler plate code from App.js. Added a component Board.js. Installed React-Bootstrap and Bootstrap.

---
+++ src/board.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Board extends Component() {
+    constructor(props) {
+        super(props);
+        this.state;
+    }
+
+    render() {
+        return <Container>
+            
+        </Container>
+    }
+}
+
+export default App;
